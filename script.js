b11669a87d57f6c3e2f48f0ca4738d3766251148
Ernest Simlat <74451245+ErnSim@users.noreply.github.com>
2020-11-15 03:11:38 +0100

    Poprawnie działający scroll

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var item = document.getElementsByTagName('MAIN')[0];
 
 window.addEventListener('wheel', function(e) {
 
-  if (e.deltaY > 0) item.scrollLeft += 1;
-  else item.scrollLeft -= 1;
+  if (e.deltaY > 0) item.scrollLeft += 1000;
+  else item.scrollLeft -= 1000;
 });
